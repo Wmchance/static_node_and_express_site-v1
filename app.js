e4225059b25a5285557c08d9cb0f0ca5dd917407
Will <wmchance@gmail.com>
2022-09-15 15:34:22 +0700

    Setup server, routes & middleware. .id num->string

---
+++ app.js
@@ -0,0 +1,45 @@
+// Import Express and set up the app
+const express = require('express');
+const app = express();
+
+// Import data.json
+const { projects } = require('./data.json');
+
+// Additional dependencies - 'cookie-parser' & 'body-parser'
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+// view engine setup
+app.set('view engine', 'pug');
+
+// Add static middleware
+app.use('/static', express.static('public'));
+
+/*
+* Routes 
+*/
+
+// 'Index/home' route
+router.get('/', function(req, res, next) {
+    res.render('index', { projects });
+});
+
+// 'About' route
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+
+// Project routes
+router.get('/projects/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId );
+    res.render('project', { project });
+  });
+
+// Start server
+app.listen(3000, () => {
+    console.log('The app is running on localhost: 3000')
+});
